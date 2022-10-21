feccdf8fc403753faf64c0548cf0945f2a934710
Jaemin <ghdckdwn8456@gmail.com>
2022-10-21 19:00:22 +0900

    Add FirstTime

---
+++ PortFolio/Assets/Scripts/Utill/GameSettings.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSettings : Singleton_DontDestroy<GameSettings>
+{
+    // Start is called before the first frame update
+    protected override void OnAwake()
+    {
+        Application.targetFrameRate = 500;
+#if UNITY_EDITOR
+        Debug.unityLogger.logEnabled = true;
+#else
+        Debug.unityLogger.logEnabled = false;
+#endif
+    }
+}
