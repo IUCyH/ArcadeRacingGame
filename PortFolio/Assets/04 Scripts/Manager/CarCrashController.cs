ee73fb06be8045e6f3d999ae182e2633034da882
Jaemin <ghdckdwn8456@gmail.com>
2022-11-02 20:56:02 +0900

    폴더 정리
    모든 에셋들 폴더로 각각 정리

---
+++ PortFolio/Assets/04 Scripts/Manager/CarCrashController.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarCrashController : MonoBehaviour
+{
+    [SerializeField]
+    Rigidbody m_playerRb;
+    void OnCollisionEnter(Collision collision)
+    {
+
+    }
+}
